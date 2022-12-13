588abe8c4e2de14de45240a7ea04571bf28878b1
Mah <marinalarissacarpesrohrig@gmail.com>
2022-12-13 11:07:47 -0300

    busca pelo cep digitado e mostra o objeto

--- javascript/Semana1/exercicio4/script.js
+++ javascript/Semana1/exercicio4/script.js
@@ -3,17 +3,16 @@ const btn = document.getElementById("button");
 const div = document.getElementById("result");
 
 btn.addEventListener("click", function () {
-    cep=88058185;
+    cep = input.value;
     console.log(cep);
-    
+
     fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`)
-    .then(response => response.json())
-    .then(dados => {
-        div.innerHTML = JSON.stringify(dados);
-       
-    }
-    );
-    
+        .then(response => response.json())
+        .then(dados => {
+                div.innerHTML = JSON.stringify(dados);
+            }
+        );
+
 })
 
 
